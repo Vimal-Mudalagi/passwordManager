b54f8d1090d940aa5fada86285345ef6f089c5a4
Vimal Mudalagi <vimalmudalagi123@gmail.com>
2024-03-18 19:35:14 +0530

    added navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      
-    
-    <h1 className='bg-purple-300'>heyyyyyyyyyyyyyyyy</h1>
+      <Navbar />
     </>
   )
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='bg-purple-200'>passwordManager
+        <div className='logo font-bold'></div>
+      <ul>
+        <li>
+            <a href= '/'>Home</a>
+            <a href= '/'>About</a>
+            <a href= '/'>Contact Us</a>
+           
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
