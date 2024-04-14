3bd0fc5257867e98429ff89ca84dbdc85d94cec4
Vimal Mudalagi <vimalmudalagi123@gmail.com>
2024-04-14 12:08:50 +0530

    added svg

--- src/Components/Manager.jsx
+++ src/Components/Manager.jsx
@@ -135,16 +135,16 @@ const Manager = () => {
 
 
             <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-            <div className="mycontainer">
+            <div className="p-2 md:p-0 md:mycontainer">
                 <h1 className=' text-white text-4xl font-bold text-center'><span className='text-green-500'>&lt;</span>
                     password
                     <span className='text-green-500'>Manager/&gt;</span></h1>
                 <p className='text-white text-2xl py-4 text-center'>Your Own Password Manager</p>
 
                 <div className="text-black flex flex-col p-4 gap-5 items-center">
-                    <input value={form.site} onChange={handleChange} placeholder='Enter the website URL' className='rounded-full border border-green-500 w-full p-4 py-2' type="text" name='site' />
-                    <div className="flex w-full justify-between gap-3">
-                        <input value={form.username} onChange={handleChange} placeholder='Enter username' className='rounded-full border border-green-500 w-full p-4 py-2' type="text" name='username' />
+                    <input value={form.site} onChange={handleChange} placeholder='Enter the website URL' className='rounded-full border border-green-500 w-full p-4 py-2' type="text" name='site' id='site'/>
+                    <div className="flex flex-col md:flex-row w-full justify-between gap-3">
+                        <input value={form.username} onChange={handleChange} placeholder='Enter username' className='rounded-full border border-green-500 w-full p-4 py-2' type="text" name='username' id='username'/>
 
                         <div className="relative">
 
